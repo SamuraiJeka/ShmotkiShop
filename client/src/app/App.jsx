c68ac3b35d3c1fe91a14984b0275c65adc46c618
SamuraiJeka <offissit123321@gmail.com>
2025-04-28 15:29:42 +0300

    reinit react-app

---
+++ client/src/app/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {RouterProvider} from "react-router-dom";
+import './App.css'
+import { router } from './router/router';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
